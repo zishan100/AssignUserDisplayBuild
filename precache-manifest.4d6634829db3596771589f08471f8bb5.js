af2fd578a013d7fb8d3dba1bb8eb340881519be2
zishan100 <Zishanansari111@gmail.com>
2021-06-16 15:55:52 +0530

    Updates

---
+++ precache-manifest.4d6634829db3596771589f08471f8bb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "353909eca6be147df234",
+    "url": "/AssignUserDisplayBuild/static/css/main.a1f8c257.chunk.css"
+  },
+  {
+    "revision": "353909eca6be147df234",
+    "url": "/AssignUserDisplayBuild/static/js/main.353909ec.chunk.js"
+  },
+  {
+    "revision": "39fb760f62aa68105ab4",
+    "url": "/AssignUserDisplayBuild/static/js/1.39fb760f.chunk.js"
+  },
+  {
+    "revision": "a3b4a695c9eac4f5899b",
+    "url": "/AssignUserDisplayBuild/static/js/runtime~main.a3b4a695.js"
+  },
+  {
+    "revision": "5c85a522b921f2542b054afaa3a8a317",
+    "url": "/AssignUserDisplayBuild/index.html"
+  }
+];
